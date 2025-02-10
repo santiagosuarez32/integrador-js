deb6b97fd70b469d5de853c1cfe6c9bca509b67c
santiagosuarez32 <dlsantii86@gmail.com>
2025-02-10 19:44:19 -0300

    dsfs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  build: {
+    outDir: "dist",
+    plugins: [tailwindcss()],
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
 });
