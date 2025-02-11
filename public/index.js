442aee4f5753b5f26386fe7d762c379bca4ea63e
santiagosuarez32 <dlsantii86@gmail.com>
2025-02-11 13:23:07 -0300

    fsdf

--- public/index.js
+++ public/index.js
@@ -7,6 +7,15 @@ const productsCart = document.querySelector(".cart-container");
 const closeBtn = document.querySelector(".close-btn");
 const deleteBtn = document.querySelector(".delete-btn");
 const buyBtn = document.querySelector(".buy-btn");
+const registerForm = document.getElementById("register-form");
+const nameInput = document.getElementById("input-name");
+const lastNameInput = document.getElementById("input-lastname");
+const emailInput = document.getElementById("input-email");
+const phoneInput = document.getElementById("input-phone");
+const msgInput = document.getElementById("input-msg");
+
+const MIN_CHARACTERS = 3;
+const MAX_CHARACTERS = 25;
 /* Carrito Desktop */
 const cartMenu = document.querySelector(".cart");
 const overlay = document.querySelector(".overlay");
@@ -16,6 +25,38 @@ const hambMenu = document.querySelector(".open-hamb");
 const menu = document.querySelector(".hamb-menu");
 const total = document.querySelector(".total");
 const successModal = document.querySelector(".add-modal");
+
+const isEmpty = (input) => {
+  console.log(!input.value.trim().length);
+  return !input.value.trim().length;
+};
+
+checkTextInput = (input) => {
+  let valid = false;
+
+  if (isEmpty(input)) {
+    console.log("esta vacio");
+    return;
+  }
+
+  if (!isBetween(input, MIN_CHARACTERS, MAX_CHARACTERS)) {
+    console.log("el campo tiene que tener entre 3 y 25 caracteres");
+    return;
+  }
+
+  valid = true;
+  return valid;
+};
+
+const isBetween = (input, min, max) => {
+  return input.value.length >= min && input.value.length <= max;
+};
+
+const showError = (input, message) => {
+  console.log(input, message);
+};
+
+const showSuccess = () => {};
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
@@ -281,6 +322,7 @@ const init = () => {
   productsCart.addEventListener("click", handleQuantity);
   deleteBtn.addEventListener("click", deleteCart);
   buyBtn.addEventListener("click", completeBuy);
+  nameInput.addEventListener("input", () => checkTextInput(nameInput));
 };
 
 init();
